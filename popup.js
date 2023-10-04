52c3921e0da7309cff57a782a5c697088da78e1b
Jayant Raj <jayantr7@gmail.com>
2023-10-04 16:41:06 -0400

    Initial commit

---
+++ popup.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var conversation = document.getElementById("conversation");
+    var userInput = document.getElementById("user-input");
+    var submitButton = document.getElementById("submit-button");
+  
+    userInput.addEventListener("keyup", function(event) {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        submitButton.click();
+      }
+    });
+  
+    submitButton.addEventListener("click", function() {
+      var userMessage = userInput.value;
+      conversation.innerHTML += "<p>User: " + userMessage + "</p>";
+      userInput.value = "";
+    });
+
+    function chat_with_chatbot(message) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost:5000/chatbot", false);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify({ conversation: message }));
+        var response = JSON.parse(xhr.responseText);
+        return response.response;
+      }
+      
+  });
+ 

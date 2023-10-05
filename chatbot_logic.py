dc07ee67912c7ae0dbdc67a4829d4a40f63b1083
Jayant Raj <jayantr7@gmail.com>
2023-10-05 12:34:58 -0400

    basic langchain memory
    the most basic langchain memory aware

--- chatbot_logic.py
+++ chatbot_logic.py
@@ -1,5 +1,10 @@
 import openai
 import configparser
+import os
+from langchain.llms import OpenAI
+from langchain.memory import ConversationBufferMemory, ConversationSummaryBufferMemory
+from langchain.chains import ConversationChain
+
 
 # Load API key from config.ini
 config = configparser.ConfigParser()
@@ -9,9 +14,18 @@ except Exception as e:
     print("An error occurred:", e)
 
 openai.api_key = config['openai']['api_key']
+os.environ["OPENAI_API_KEY"] = config['openai']['api_key']
 
 context = [{'role':'system', 'content':'You are the SaatvaAI Chatbot. Your job is to help users about the saatva.com website. Be concise, use as few tokens as possible, be friendly.'},
             ]
+
+llm = OpenAI()
+memory = ConversationBufferMemory()
+conversation = ConversationChain(
+   llm=llm,
+   memory=memory,
+   verbose=False
+)
     
 def call_LLM_with_memory(context):
    response = openai.ChatCompletion.create(
@@ -23,10 +37,12 @@ def call_LLM_with_memory(context):
    return response.choices[0].message["content"]
 
 '''chat_with_chatbot() is the entry point to the chatbot logic'''
-def chat_with_chatbot(conversation, currentURL, currScreenHTMLContent):
-    context.append({'role':'user', 'content':f"{conversation}"})
-    response = call_LLM_with_memory(context)
-    context.append({'role':'assistant', 'content':f"{response}"})
+def chat_with_chatbot(userPrompt, currentURL, currScreenHTMLContent):
+    response = conversation.predict(input=str(userPrompt))
+    
+    #context.append({'role':'user', 'content':f"{userPrompt}"})
+    #response = call_LLM_with_memory(context)
+    #context.append({'role':'assistant', 'content':f"{response}"})
 
     # Save screenGrab to a file
     with open('screen_grab.txt', 'w') as f:

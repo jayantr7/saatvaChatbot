f816639f8601f69cd22189ff21a513c7f9358dda
Jayant Raj <jayantr7@gmail.com>
2023-10-05 12:11:09 -0400

    basic context awareness
    the most basic version of giving the chatbot API memory.

--- chatbot_logic.py
+++ chatbot_logic.py
@@ -10,27 +10,27 @@ except Exception as e:
 
 openai.api_key = config['openai']['api_key']
 
+context = [{'role':'system', 'content':'You are the SaatvaAI Chatbot. Your job is to help users about the saatva.com website. Be concise, use as few tokens as possible, be friendly.'},
+            ]
+    
+def call_LLM_with_memory(context):
+   response = openai.ChatCompletion.create(
+       model='gpt-3.5-turbo',
+       messages=context,
+       max_tokens=50,
+       temperature=0.3,
+   )
+   return response.choices[0].message["content"]
+
+'''chat_with_chatbot() is the entry point to the chatbot logic'''
 def chat_with_chatbot(conversation, currentURL, currScreenHTMLContent):
-    try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "user", "content":str(conversation + " Current URL: " + currentURL)}
-                ],
-            max_tokens=50,
-            temperature=0.3,
-        )
-        return response['choices'][0]['message']['content']
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
-    
-    
-    # For testing purposes, a simple example response is provided
-    response = "Current URL is: " + str(currentURL)
-    
+    context.append({'role':'user', 'content':f"{conversation}"})
+    response = call_LLM_with_memory(context)
+    context.append({'role':'assistant', 'content':f"{response}"})
+
     # Save screenGrab to a file
     with open('screen_grab.txt', 'w') as f:
         f.write(currScreenHTMLContent)
-
-    return response
+    
+    finalReply = response
+    return finalReply
